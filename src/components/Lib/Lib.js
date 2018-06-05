d2920ec0e4ecfde8d8378b834dfdd24f4d614f6f
moonman <moonman@syneforge.com>
2018-06-05 18:53:35 +0300

    some

--- src/components/Lib/Lib.js
+++ src/components/Lib/Lib.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 //components
 import LibItem from '../LibItem/LibItem'
 import book1 from '../../img/1984.jpg'
+import book2 from '../../img/1984(2).jpg'
 
 
 class Lib extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            lib: [
+                {
+                    bookName: 'book 1 in lib',
+                    favorite: true,
+                    bookImg: book1
+                },{
+                    bookName: 'book 2 in lib',
+                    favorite: false,
+                    bookImg: book2
+                },
+            ]
+
 
         };
     }
@@ -18,8 +31,8 @@ class Lib extends Component {
     render() {
         return (
             <section className="lib-list">
-                <LibItem bookName = "Book1" favorite = {true} bookImg = {book1}/>
-                <LibItem bookName = "Book1" favorite = {false} bookImg = {book1}/>
+                <LibItem bookName = {this.state.lib[0].bookName} favorite = {this.state.lib[0].favorite} bookImg = {this.state.lib[0].bookImg}/>
+                <LibItem bookName = {this.state.lib[1].bookName} favorite = {this.state.lib[1].favorite} bookImg = {this.state.lib[1].bookImg}/>
             </section>
 
         );
